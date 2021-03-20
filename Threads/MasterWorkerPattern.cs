1c9e3d78da05da6979857d45a50a5091e6761c94
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2021-03-20 14:06:52 +0100

    Threads

---
+++ Threads/MasterWorkerPattern.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MasterWorker_Patter
+{
+    class Program
+    {
+        static void Start(string[] args)
+        {
+            int max = 1000;
+            int[] numbers = new int[max];
+            bool[] results = new bool[max];
+            int position = 0;
+
+            Random r = new Random();
+
+            for (int i = 0; i < max; i++)
+            {
+                numbers[i] = r.Next();
+            }
+
+            int threadCount = 10;
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                Worker w = new Worker(ref numbers, ref results);
+
+                Thread t = new Thread(() => w.DoWork(ref position));
+                threads.Add(t);
+                t.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine(string.Join(":", results));
+        }
+    }
+
+    internal class Worker
+    {
+        public static object position_lock = new();
+        public static object result_lock = new();
+
+        private int[] numbers;
+        private bool[] results;
+
+        public Worker(ref int[] numbers, ref bool[] results)
+        {
+            this.numbers = numbers;
+            this.results = results;
+        }
+
+
+        public void DoWork(ref int position)
+        {
+            while (position < numbers.Length)
+            {
+                int cp = -1;
+                lock (position_lock)
+                {
+                    cp = position;
+                    position++;
+                }
+
+                Console.WriteLine($"CP: {cp}");
+
+                //Do Stuff Parallel
+                bool result = numbers[cp] % 2 == 0;
+
+                lock (result_lock)
+                {
+                    results[cp] = result;
+                }
+            }
+        }
+    }
+}
---
+++ Threads/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Worker w1 = new Worker();
+            Worker w2 = new Worker(ref sum);
+
+            Thread t1 = new Thread(() => w1.DoWork(ref sum)); //Methode mit params in lambda
+            Thread t2 = new Thread(w2.DoWorkNoParams); //Methode ohne geht auch so
+
+            //starte threat
+            t1.Start();
+            while (!t1.IsAlive) ; //-> sinnvoll, wenn man warten muss, bis thread gestartet ist
+            t2.Start();
+
+            //wartet auf beenden von thread
+            t1.Join();
+            t2.Join();
+
+
+            //Task ist wie thread nur besser
+            //startet eine Task
+            Task T = Task.Run(w1.DoWorkNoParams);
+            //wartet auf beenden
+            T.Wait();
+        }
+    }
+
+    class Worker
+    {
+        //Mutex sperrt zugriff auf diesen bereich
+        public static object mutex_lock = new();
+
+        private int sumLocal;
+
+        public Worker()
+        {
+        }
+
+        public Worker(ref int sum)
+        {
+            this.sumLocal = sum;
+        }
+
+        public void DoWork(ref int sum)
+        {
+            //wichtig!
+            lock (mutex_lock)
+            {
+                sum++;
+            }
+        }
+
+        public void DoWorkNoParams()
+        {
+            lock (mutex_lock)
+            {
+                sumLocal++;
+            }
+        }
+
+        private void threadPool()
+        {
+            //bestimmt anzahl an threads erstellen
+            int threadCount = 10;
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                Worker w = new Worker();
+
+                Thread t = new Thread(w.DoWorkNoParams);
+                threads.Add(t);
+                t.Start();
+            }
+
+
+            //auf alle threads warten
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+    }
+}
